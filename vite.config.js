c786153c2ffc1a96544c3699771a31b9ebd9c4d2
JimEllars <5588701+JimEllars@users.noreply.github.com>
2026-08-01 16:44:56 +0000

    Fix Vite build warnings

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,15 @@ export default defineConfig({
   },
    build: {
     outDir: 'dist',
-    sourcemap: true
+    sourcemap: true,
+    chunkSizeWarningLimit: 3000,
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === 'CIRCULAR_DEPENDENCY') return
+        if (warning.code === 'EVAL') return
+        if (warning.message.includes('is dynamically imported by')) return
+        warn(warning)
+      }
+    }
   },
 });
